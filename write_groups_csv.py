1b5f4771e07853bf34df24fcd71c8fe410bbcb54
davidfstein <davidmfstein@gmail.com>
2019-06-10 16:13:02 -0400

    removed main

--- write_groups_csv.py
+++ write_groups_csv.py
@@ -1,29 +1,18 @@
+from models import model_utils
 import csv
 
 def write_groups(groups):
     with open("groups.csv", "w+") as file:
         groupwriter = csv.writer(file)
-        header_vals = get_header_vals(groups[0].subjects[0])
+        header_vals = model_utils.get_fields(groups[0].subjects[0])
         header_vals.append("group number")
         groupwriter.writerow(header_vals)
         for i in range(len(groups)):
             for subject in groups[i].subjects:
-                vals = get_subject_field_vals(subject)
+                vals = model_utils.get_field_vals(subject)
                 vals.append(i + 1)
                 groupwriter.writerow(vals)
+            groupwriter.writerow("")
+            groupwriter.writerow("")
+            groupwriter.writerow("")
 
-def get_header_vals(subject):
-    header_vals = []
-    for item in vars(subject).items():
-        if item[0] == 'combined':
-            continue
-        header_vals.append(item[0])
-    return header_vals
-
-def get_subject_field_vals(subject):
-    vals = []
-    for item in vars(subject).items():
-        if item[0] == 'combined':
-            continue
-        vals.append(item[1])
-    return vals
